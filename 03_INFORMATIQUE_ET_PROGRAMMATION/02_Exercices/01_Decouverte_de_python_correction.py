4bbb7e05e9bb86e60910e7ee94e02dbb497495c1
EnzoFmx <enzofremeaux@gmail.com>
2023-01-17 10:20:31 +0100

    add

---
+++ 03_INFORMATIQUE_ET_PROGRAMMATION/02_Exercices/01_Decouverte_de_python_correction.py
@@ -0,0 +1,72 @@
+"""
+Partie 1 :
+>>> 5+62
+67
+>>> 7/2
+3.5
+>>> 6-5
+1
+>>> 78//67
+1
+>>> 78/67
+1.164179104477612
+>>> 5%4
+1
+"""
+"""
+Partie 2
+>>> 6+1
+7
+>>> 7-3
+4
+>>> 7--7
+14
+>>> 6//3
+2
+>>> 7//3
+2
+>>> 15%14
+1
+>>> 15%7
+1
+>>> 3/2
+1.5
+"""
+"""Exercice 2"""
+a = 10
+b = 20
+c = 67
+d = -78
+e = 'Je suis phrase'
+f = 8
+g = 14
+h = 6
+i = 34
+j = 3
+k = 34
+l = 3
+
+"""
+>>> a + b 
+30
+>>> c - d
+145
+>>> e * f
+'Je suis phraseJe suis phraseJe suis phraseJe suis phraseJe suis phraseJe suis phraseJe suis phraseJe suis phrase'
+>>> g % h
+2
+>>> i / j
+11.333333333333334
+>>> k // l
+11
+
+% => reste de la division euclidienne
+// => quotion de la division euclidienne
+/ => division décimale
+"""
+
+a = 10
+b = 6
+c = 78
+result = a + b + c
+print(result)
